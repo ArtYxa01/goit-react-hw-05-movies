0bf2f88c904527ac2d3aa94b373c23aec6ecd8c7
ArtYxa01 <115730030+ArtYxa01@users.noreply.github.com>
2023-10-13 23:01:21 +0300

    v bnm,.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,26 +8,32 @@ import { MovieDetails } from '../component/pages/MovieDetails/MovieDetails';
 // import { Reviews } from './Reviews/Reviews';
 import { PageNotFound } from '../Error/PageNotFound';
 
-const Movies = lazy(() =>
-  import('../components/pages/Movies/Movies').then(module => ({
+const Movies = () => {
+ import('../components/pages/Movies/Movies').then(module => ({
     ...module,
     default: module.Movies,
   }))
-);
+};
+export default Movies
+ import '../components/pages/Movies/Movies'
 
-const Cast = lazy(() =>
+const Cast = () => {
   import('./components/Cast/Cast').then(module => ({
     ...module,
     default: module.Cast,
   }))
-);
+}
+export default Cast
+import '../components/component/Cast/Cast'
 
-const Reviews = lazy(() =>
+const Reviews = () => {
   import('../component/Reviews/Reviews').then(module => ({
     ...module,
     default: module.Reviews,
   }))
-);
+};
+export default Reviews
+import '../components/component/Reviews/Reviews'
 
 export const App = () => {
   return (
