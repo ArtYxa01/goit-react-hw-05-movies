94e4680f2dabee19f7b7701b3de73deebcaea60d
ArtYxa01 <115730030+ArtYxa01@users.noreply.github.com>
2023-10-16 20:31:52 +0300

    xjjdcxjfch

--- src/components/component/Cast/Cast.jsx
+++ src/components/component/Cast/Cast.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getMovieCast } from 'components/services/fetchMovies';
-import { NoCast } from 'component/Error/NoCast';
+import { NoCast } from 'Error/NoCast';
 import { List, Item, Img, Name, Character } from './Cast.styled';
 
 export const Cast = () => {
