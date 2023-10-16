444951177a9016fb2f16bfdc1d833916e7816fa3
ArtYxa01 <115730030+ArtYxa01@users.noreply.github.com>
2023-10-16 21:21:02 +0300

    vczbuvberjn vikwd

--- src/components/pages/Movies/Movies.jsx
+++ src/components/pages/Movies/Movies.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { SearchBox } from 'components/component/SearchBox/SearchBox';
 import { getMovieByName } from 'components/services/fetchMovies';
-import { MoviesList } from 'component/MovieList/MoviesList';
+import { MoviesList } from 'MovieList/MovieList';
 
 export const Movies = () => {
   const [movies, setMovies] = useState([]);
